db4380e2dae7a0f8e65305a3658f6488864d27e0
Risenio <Risenio@users.noreply.github.com>
2018-12-19 07:24:10 -0500

    :cookie:
    Updated.
    Declared.
    Autoupdate-support.
    Gui thingies.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Elinify
 Highest form of QoL
+
+Makes all chars around you elins
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-module.exports = function Elinify(dispatch) {
-    dispatch.hook('S_SPAWN_USER', 13, (event) => {
-        race = Math.floor(event.templateId / 100) % 100;
-        job = event.templateId % 100;
-        if (job > 10) {
-            return;
-        }
-        if (race !== 10) {
-            event.templateId = 11000 + job;
-            return true;
-        }
-    });
-};
+module.exports = function Elinify(mod) {
+	let race, job;
+	mod.hook('S_SPAWN_USER', 14, (event) => {
+		race = Math.floor(event.templateId / 100) % 100;
+		job = event.templateId % 100;
+		if ((job > 10) || (race === 10)) return;
+		event.templateId = 11000 + job;
+		return true;
+	})
+}
